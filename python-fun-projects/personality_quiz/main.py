a35fdce2362dd57034e93e8264f646e4f5629417
Sagar Deshmukh <sagarddd@gmail.com>
2026-08-03 22:43:08 -0700

    quiz

---
+++ python-fun-projects/personality_quiz/main.py
@@ -0,0 +1,18 @@
+
+# Mini-project (30 min): Build a "Personality Quiz" — ask the user 3–4 questions 
+# (name, favorite animal, favorite color, etc.) and print a silly personalized result, like:
+
+
+#    Hi Sam! Since you love wolves and the color blue,
+#    your spirit animal energy is: Calm Nighttime Explorer 🌙
+
+
+# Wrap-up (10 min): Save the project. Talk through: what's a variable? What does input() do?
+
+
+
+
+quiz = input("Do you want to take the quiz? (yes/no): ").lower()
+list = []
+list.append(quiz)
+print(list)
